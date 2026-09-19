2839aa610e4854ad3e84a9fb939e7f76da4010c9
Sandeepa-git <agsvwimalasiri@gmail.com>
2026-09-20 01:40:25 +0530

    feat: default backendUrl to live Azure App Service

--- frontend/flutter_app/lib/core/config/env_config.dart
+++ frontend/flutter_app/lib/core/config/env_config.dart
@@ -7,12 +7,7 @@ class EnvConfig {
       return fromEnv;
     }
     
-    if (kIsWeb) {
-      return 'http://127.0.0.1:8000/api/v1';
-    } else if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:8000/api/v1';
-    } else {
-      return 'http://127.0.0.1:8000/api/v1';
-    }
+    // Default to the live hosted Azure App Service
+    return 'https://tailorsync-api-prod-gxgvdaawe5a6bffn.centralus-01.azurewebsites.net/api/v1';
   }
 }
